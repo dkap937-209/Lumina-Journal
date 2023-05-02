2e72b59ef722ed93abe495acb5b21a7f12988066
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-05-03 09:55:50 +1200

    Setup navigation graph

--- app/src/main/java/com/dk/luminajournal/MainActivity.kt
+++ app/src/main/java/com/dk/luminajournal/MainActivity.kt
@@ -3,14 +3,10 @@ package com.dk.luminajournal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.dk.luminajournal.navigation.Screen
+import com.dk.luminajournal.navigation.SetupNavGraph
 import com.dk.luminajournal.ui.theme.LuminaJournalTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +15,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             LuminaJournalTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/dk/luminajournal/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.dk.luminajournal.util
+
+object Constants{
+    const val APP_ID = "mydiaryapp-mvmjr"
+    const val CLIENT_ID = "974853111567-027h682gfgjair2r78qjp35g49bu2eks.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
